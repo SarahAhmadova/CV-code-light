ddbebf7852e2c254860b013fdc95bc4a27fe5785
SaraAhmadova <sara.ehmedova7@gmail.com>
2021-05-20 21:50:50 +0400

    add new projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 let clr5= "#1e1f23";
 
 
-let mode=document.querySelector("input[type=checkbox]");
-mode.addEventListener("click",()=>{
-    console.log(mode);
-    if(mode.checked){
-        console.log("truee");
-        // $(".box .main .header .basic-info div span").css("color","#555555");
-        $(".main").css("background-color","#fff");
+// let mode=document.querySelector("input[type=checkbox]");
+// mode.addEventListener("click",()=>{
+//     console.log(mode);
+//     if(mode.checked){
+//         console.log("truee");
+//         // $(".box .main .header .basic-info div span").css("color","#555555");
+//         $(".main").css("background-color","#fff");
 
-    }
-    else $(".main").css("background-color",clr5);
-})
-console.log();
+//     }
+//     else $(".main").css("background-color",clr5);
+// })
+let num=2;
+let h=1;
+for (let index = 0; index <5; index++) {
+    h=h*num;
+}
+    console.log(h);
